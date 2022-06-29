b284941f9652556cd134ac233a1b257aaea9fc13
Ozodbek <ozodbek.xomidbekov@gmail.com>
2022-06-29 17:23:31 +0900

    migrate to Postres and open api

--- src/test/java/com/example/server/ServerApplicationTests.java
+++ src/test/java/com/example/server/ServerApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.server;
 
+import com.example.server.config.SwaggerConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+
 class ServerApplicationTests {
 
 	@Test
